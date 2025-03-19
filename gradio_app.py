1d0ee2f09fadda3bb7d56ec1c7adf26e9f82e3ca
Zeqiang-Lai <916323301@qq.com>
2025-03-20 00:12:11 +0800

    gradio: default to mc for better compatibility

--- gradio_app.py
+++ gradio_app.py
@@ -651,7 +651,7 @@ if __name__ == '__main__':
     parser.add_argument('--port', type=int, default=8080)
     parser.add_argument('--host', type=str, default='0.0.0.0')
     parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--mc_algo', type=str, default='dmc')
+    parser.add_argument('--mc_algo', type=str, default='mc')
     parser.add_argument('--cache-path', type=str, default='gradio_cache')
     parser.add_argument('--enable_t23d', action='store_true')
     parser.add_argument('--disable_tex', action='store_true')
